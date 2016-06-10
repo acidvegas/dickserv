2a09f8393c6602c2d6e290559ae02b2756664948
Dionysus <acidvegas@users.noreply.github.com>
2016-06-10 08:02:50 -0400

    Create youtube.py

---
+++ dickserv/modules/youtube.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# DickServ IRC Bot
+# Developed by acidvegas in Python 3.5
+# https://github.com/acidvegas/dickserv/
+# youtube.py
+
+import re
+
+import config
+import httplib
+
+def check(url):
+    return re.match('^(http(s?):\/\/)?(www\.)?youtu(be)?\.([a-z])+\/(watch(.*?)(\?|\&)v=)?(.*?)(&(.)*)?$', url)
+
+def title(url):
+    video_id = check(url).group(9)
+    api      = 'http://www.googleapis.com/youtube/v3/videos?key=AIzaSyC6lTRPIY-6P0zcpMggqY9mVEC2ZvXUcas&part=snippet,contentDetails,statistics&id=%s' % video_id
+    data     = httplib.get_json(api)
+    return data['items'][0]['snippet']['title']
+
+def search(query):
+    api    = 'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyC6lTRPIY-6P0zcpMggqY9mVEC2ZvXUcas&q=%s&maxResults=10&&fields=items' % httplib.data_quote(query)
+    data   = httplib.get_json(api)
+    results = {}
+    for item in data['items']:
+        title   = item['snippet']['title']
+        content = item['id']['kind']
+        if   content == 'youtube#channel'  : url = 'https://www.youtube.com/channel/' + item['id']['channelId']
+        elif content == 'youtube#playlist' : url = 'https://www.youtube.com/playlist?list=' + item['id']['playlistId']
+        elif content == 'youtube#video'    : url = 'https://www.youtube.com/watch?v=' + item['id']['videoId']
+        results[title] = url
+    return results
