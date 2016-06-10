902cbb5e0622d43d7f77a04ddabc8011abc83924
Dionysus <acidvegas@users.noreply.github.com>
2016-06-10 07:54:14 -0400

    Create config.py

---
+++ dickserv/core/config.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# DickServ IRC Bot
+# Developed by acidvegas in Python 3.5
+# https://github.com/acidvegas/dickserv/
+# config.py
+
+#IRC Server Information
+server  = 'irc.server.com'
+port    = 6697
+use_ssl = True
+channel = '#dev'
+key     = None
+
+#IRC Identity Information
+nickname = 'DickServ'
+username = 'dickserv'
+realname = 'Dickserv IRC Bot'
+password = None
+
+# Login Passwords
+nickserv = None
+operserv = None
+
+# GLOBALS (DO NOT EDIT)
+start_time = 0
+last_time  = 0
