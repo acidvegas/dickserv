8611a1d98f581ef05df66d2bf367979db7b8717f
Dionysus <acidvegas@users.noreply.github.com>
2016-06-10 08:01:52 -0400

    Create isup.py

---
+++ dickserv/modules/isup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# DickServ IRC Bot
+# Developed by acidvegas in Python 3.5
+# https://github.com/acidvegas/dickserv/
+# isup.py
+
+import httplib
+
+def check(url):
+    source = httplib.get_source('http://isup.me/' + url)
+    if   source.find('It\'s just you.') != -1     : return 'UP'
+    elif source.find('It\'s not just you!') != -1 : return 'DOWN'
+    elif source.find('Huh?') != -1                : return 'INVALID'
+    else                                          : return 'UNKNOWN'
