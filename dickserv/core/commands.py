f098a24511f099971ba031c0a0cc2ab40a19aa38
Dionysus <acidvegas@users.noreply.github.com>
2016-06-10 07:52:04 -0400

    Create commands.py

---
+++ dickserv/core/commands.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# DickServ IRC Bot
+# Developed by acidvegas in Python 3.5
+# https://github.com/acidvegas/dickserv/
+# commands.py
+
+import cryptocurrency
+import dictionary
+import fml
+import imdb
+import isup
+import reddit
+import wolfram
+import youtube
