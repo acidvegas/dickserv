b34fe5693af7d4788c75c3dfb025f5b7963b1daf
Dionysus <acidvegas@users.noreply.github.com>
2016-07-19 00:45:13 -0400

    Update ascii.py

--- dickserv/data/ascii.py
+++ dickserv/data/ascii.py
@@ -1,1 +1,6 @@
-#
+import glob, os
+with open('list.txt', 'w') as ascii_file:
+    count = 1
+    for i in glob.glob('ascii/*.txt'):
+        ascii_file.write('%d. %s\n' % (count, os.path.basename(i).replace('.txt', '', 1)))
+        count += 1
