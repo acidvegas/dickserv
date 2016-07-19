1524269dba09dfa8741b90e7df87ddcf6dd53415
Dionysus <acidvegas@users.noreply.github.com>
2016-07-19 00:30:16 -0400

    Update ascii.py

--- dickserv/modules/ascii.py
+++ dickserv/modules/ascii.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 # DickServ IRC Bot
-# Developed by acidvegas in Python 3.5
+# Developed by acidvegas in Python 3
 # https://github.com/acidvegas/dickserv/
 # ascii.py
 
+import glob
 import os
+import random
+
+ascii_dir = os.getcwd() + '/data/ascii'
 
 def read(file_name):
-    ascii_file = '%s/ascii/%s.txt' % (os.getcwd(), file_name)
+    if file_name == 'random':
+        ascii_list = glob.glob(ascii_dir + '/*')
+        ascii_file = random.choice(ascii_list)
+    else:
+        ascii_file = '%s/%s.txt' % (ascii_dir, file_name)
     if os.path.isfile(ascii_file):
-        return open(ascii_file, 'r').readlines()
+        return open(ascii_file, 'r').readlines() + [ascii_file.replace('/home/super/bots/dickserv/dickserv/data/ascii/', '', 1),]
     else:
         return False
