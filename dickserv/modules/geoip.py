5568892cfca64d3f16a1f6af459777750a124051
Dionysus <acidvegas@users.noreply.github.com>
2016-07-19 00:31:53 -0400

    Create geoip.py

---
+++ dickserv/modules/geoip.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# DickServ IRC Service Bot
+# Developed by acidvegas in Python 3
+# https://github.com/acidvegas/dickserv/
+# geoip.py
+
+import httplib
+
+def lookup(ip):
+    api = httplib.get_json('http://freegeoip.net/json/' + ip)
+    if api['city'] and api['region_name'] and api['country_name']:
+        return '%s, %s, %s' % (api['city'], api['region_name'], api['country_name'])
+    elif api['country_name']:
+        return api['country_name']
+    else:
+        return False
