5230131d81fc3d0d7d95de8c4c02fe71ba4ea2e2
Dionysus <acidvegas@users.noreply.github.com>
2016-07-19 00:20:49 -0400

    Update dickserv.py

--- dickserv/dickserv.py
+++ dickserv/dickserv.py
@@ -4,39 +4,23 @@
 # https://github.com/acidvegas/dickserv/
 # dickserv.py
 
-'''
-ISC License
-
-Copyright (c) 2016, acidvegas (https://github.com/acidvegas/)
-
-Permission to use, copy, modify, and/or distribute this software for any
-purpose with or without fee is hereby granted, provided that the above
-copyright notice and this permission notice appear in all copies.
-
-THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-'''
-
 import os
 import sys
 
 sys.dont_write_bytecode = True
 os.chdir(sys.path[0] or '.')
-sys.path += ['core', 'modules']
+sys.path += ('core', 'modules')
 
 import debug
 import irc
 
 debug.info()
-if not debug.get_windows():
-    if debug.check_root():
-        debug.error_exit('Do not run DickServ as root!')
 if not debug.check_version(3):
     debug.error_exit('DickServ requires Python 3!')
+if debug.check_windows():
+    debug.error_exit('DickServ can not be executed on a Windows OS!')
+if debug.check_root():
+    debug.error_exit('Do not run DickServ as root!')
+debug.check_libs()
 irc.DickServ.connect()
 debug.keep_alive()
