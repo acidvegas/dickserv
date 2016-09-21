7804334548ca00356a0e0a2711feb7ebe469b3a2
Dionysus <acidvegas@users.noreply.github.com>
2016-09-21 06:28:41 -0400

    Update unreal.py

--- dickserv/modules/unreal.py
+++ dickserv/modules/unreal.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # DickServ IRC Bot
 # Developed by acidvegas in Python 3
-# https://github.com/acidvegas/dickserv/
+# https://github.com/acidvegas/dickserv
 # unreal.py
 
 import threading
@@ -15,20 +15,22 @@ import irc
 def check_update():
     source = httplib.get_source('https://www.unrealircd.org/docs/FAQ')
     latest = functions.between(source, 'The latest <b>Stable</b> version is <b>', '</b> which was released on ')
-    if config.ircd_version != latest:
+    if current_version != latest:
         return latest
     else:
         return False
+
+def current_version():
+    version = os.popen('~/unreal/./unrealircd version').read()
+    return version[11:].split()[0]
 
 class loop(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
     def run(self):
         while True:
-            hour   = time.strftime('%I')
-            minute = time.strftime('%M')
-            if hour == 12 and minute == 00:
+            if time.strftime('%I') == 12:
                 update = check_update()
                 if update:
-                    irc.DickServ.sendmsg('UnrealIRCd version %s has been released.' % update)
-            time.sleep(45)
+                    irc.DickServ.sendmsg(config.channel, 'UnrealIRCd version %s has been released.' % update)
+            time.sleep(45*60)
