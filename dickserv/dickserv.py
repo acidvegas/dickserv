3e0e77220d735e66841dad4e646cc54b4632f42d
Dionysus <acidvegas@users.noreply.github.com>
2016-09-21 05:22:55 -0400

    Update dickserv.py

--- dickserv/dickserv.py
+++ dickserv/dickserv.py
@@ -18,9 +18,10 @@ debug.info()
 if not debug.check_version(3):
     debug.error_exit('DickServ requires Python 3!')
 if debug.check_windows():
-    debug.error_exit('DickServ can not be executed on a Windows OS!')
-if debug.check_root():
-    debug.error_exit('Do not run DickServ as root!')
+    if debug.check_admin():
+        debug.error_exit('Do not run DickServ as an administrator!')
+else:
+    if debug.check_root():
+        debug.error_exit('Do not run DickServ as root!')
 debug.check_libs()
 irc.DickServ.connect()
-debug.keep_alive()
