413f24c3246354ac7268d3417b82f2d2536022e4
Dionysus <acidvegas@users.noreply.github.com>
2016-09-21 05:21:57 -0400

    Update debug.py

--- dickserv/core/debug.py
+++ dickserv/core/debug.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python
 # DickServ IRC Bot
 # Developed by acidvegas in Python 3
-# https://github.com/acidvegas/dickserv/
+# https://github.com/acidvegas/dickserv
 # debug.py
 
+import ctypes
 import os
 import sys
 import time
 
 import config
 
+def check_admin():
+    if ctypes.windll.shell32.IsUserAnAdmin() != 0:
+        return True
+    else:
+        return False
+        
 def check_libs():
-    try                : import bs4
-    except ImportError : error_exit('Missing required \'bs4\' library. (https://pypi.python.org/pypi/beautifulsoup4)')
+    try:
+        import bs4
+    except ImportError:
+        error_exit('Missing required \'bs4\' library. (https://pypi.python.org/pypi/beautifulsoup4)')
 
 def check_root():
     if os.getuid() == 0 or os.geteuid() == 0:
@@ -39,8 +48,10 @@ def clear():
         os.system('clear')
 
 def error(msg, reason=None):
-    if reason : print('%s | [!] - %s (%s)' % (get_time(), msg, str(reason)))
-    else      : print('%s | [!] - %s'      % (get_time(), msg))
+    if reason:
+        print('%s | [!] - %s (%s)' % (get_time(), msg, str(reason)))
+    else:
+        print('%s | [!] - %s' % (get_time(), msg))
 
 def error_exit(msg):
     raise SystemExit('%s | [!] - %s' % (get_time(), msg))
@@ -48,47 +59,15 @@ def error_exit(msg):
 def get_time():
     return time.strftime('%I:%M:%S')
 
-def help():
-    return '''@help                 Information about the commands.
-.ascii list           A list of all the ASCII art files.
-.ascii random         Display a random ASCII art file.
-.ascii <name>         Display the <name> ASCII art file.
-.btc                  Bitcoin rate in USD.
-.date                 Get the current date and time.
-.define <word>        Get the definition of <word>.
-.dickserv             Information about the bot.
-.filter enable        Enable word filters.
-.filter disable       Disable word filters.
-.geoip <ip>           Geographical location information about <ip>.
-.imdb <query>         Search IMDb and return the 1st result for <search>.
-.isup <url>           Check if <url> is up or not.
-.ltc                  Litecoin rate in USD.
-.r <subreddit>        Read top posts from <subreddit>
-.remind <time> <text> Remind yourself about <text> in <time>.
-.resolve <ip/url>     Resolve <ip/url> to a hostname or IP address.
-.steam <query>        Search <query> on the Steam store.
-.talent               RIP DITTLE DIP DIP DIP DIP IT\'S YA BIRTHDAY!!1@11!
-.tpb <query>          Search <query> on ThePirateBay.
-.ud <word>            Get the urban dictionary definition of <word>.
-.uptime               Get the amount of time DickServ has been running.
-.wolfram <ask>        Get the results of <query> from WolframAlpha.
-.yt <query>           Search <query> on YouTube.'''
-
 def info():
     clear()
     print(''.rjust(56, '#'))
-    print('#' + ''.center(54) + '#')
-    print('#' + 'DickServ IRC Bot'.center(54) + '#')
-    print('#' + 'Developed by acidvegas in Python 3'.center(54) + '#')
-    print('#' + 'https://github.com/acidvegas/dickserv/'.center(54) + '#')
-    print('#' + ''.center(54) + '#')
+    print('#%s#' % ''.center(54))
+    print('#%s#' % 'DickServ IRC Bot'.center(54))
+    print('#%s#' % 'Developed by acidvegas in Python 3'.center(54))
+    print('#%s#' % 'https://github.com/acidvegas/dickserv'.center(54))
+    print('#%s#' % ''.center(54))
     print(''.rjust(56, '#'))
 
 def irc(msg):
     print('%s | [~] - %s' % (get_time(), msg))
-
-def keep_alive():
-    try:
-        while True : input('')
-    except KeyboardInterrupt:
-        sys.exit()
