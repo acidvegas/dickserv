3a1742fffeb29fcabb1a7d6cf25b25c2eca54fb7
Dionysus <acidvegas@users.noreply.github.com>
2016-10-10 21:42:49 -0400

    Update youtube.py

--- dickserv/modules/youtube.py
+++ dickserv/modules/youtube.py
@@ -13,19 +13,22 @@ def check(url):
 
 def title(url):
     video_id = check(url).group(9)
-    api      = 'https://www.googleapis.com/youtube/v3/videos?key=AIzaSyC6lTRPIY-6P0zcpMggqY9mVEC2ZvXUcas&part=snippet,contentDetails,statistics&id=%s' % video_id
+    api      = 'https://www.googleapis.com/youtube/v3/videos?key=AIzaSyC6lTRPIY-6P0zcpMggqY9mVEC2ZvXUcas&part=snippet,contentDetails,statistics&id=' + video_id
     data     = httplib.get_json(api)
     return data['items'][0]['snippet']['title']
 
 def search(query):
-    api     = 'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyC6lTRPIY-6P0zcpMggqY9mVEC2ZvXUcas&q=%s&maxResults=10&&fields=items' % httplib.data_quote(query)
+    api     = 'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyC6lTRPIY-6P0zcpMggqY9mVEC2ZvXUcas&q={0}&maxResults=10&&fields=items'.format(httplib.data_quote(query))
     data    = httplib.get_json(api)
     results = {}
     for item in data['items']:
         title   = item['snippet']['title']
         content = item['id']['kind']
-        if   content == 'youtube#channel'  : url = 'https://www.youtube.com/channel/' + item['id']['channelId']
-        elif content == 'youtube#playlist' : url = 'https://www.youtube.com/playlist?list=' + item['id']['playlistId']
-        elif content == 'youtube#video'    : url = 'https://www.youtube.com/watch?v=' + item['id']['videoId']
+        if content == 'youtube#channel':
+            url = 'https://www.youtube.com/channel/' + item['id']['channelId']
+        elif content == 'youtube#playlist':
+            url = 'https://www.youtube.com/playlist?list=' + item['id']['playlistId']
+        elif content == 'youtube#video':
+            url = 'https://www.youtube.com/watch?v=' + item['id']['videoId']
         results[title] = url
     return results
