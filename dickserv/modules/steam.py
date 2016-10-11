53a447e78c0d727a527b688ae27d86f624c20361
Dionysus <acidvegas@users.noreply.github.com>
2016-10-10 21:52:11 -0400

    Update steam.py

--- dickserv/modules/steam.py
+++ dickserv/modules/steam.py
@@ -13,11 +13,13 @@ def search(query):
     soup  = BeautifulSoup(api)
     games = soup.find_all('span', {'class':'title'})
     games = [game.text for game in games]
-    urls  = soup.find_all('a',    {'class':'search_result_row ds_collapse_flag'})
+    urls  = soup.find_all('a', {'class':'search_result_row ds_collapse_flag'})
     urls  = ['https://store.steampowered.com/app/' + url['data-ds-appid'] for url in urls]
     if games and urls:
-        if len(games) > 10 : games = games[:10]
-        if len(urls)  > 10 : urls = urls[:10]
+        if len(games) > 10:
+            games = games[:10]
+        if len(urls) > 10:
+            urls = urls[:10]
         return dict(zip(games, urls))
     else:
         return False
