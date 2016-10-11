61502784b496fa2dcdef5981bdceb2a1fdde0f34
Dionysus <acidvegas@users.noreply.github.com>
2016-10-10 21:47:48 -0400

    Update unreal.py

--- dickserv/modules/unreal.py
+++ dickserv/modules/unreal.py
@@ -4,18 +4,6 @@
 # https://github.com/acidvegas/dickserv
 # unreal.py
 
-'''
-This module is to check for updates on the UnrealIRCd.
-
-To enable this module, make the following changes:
-
-core/irc.py      | Add "unreal.loop().start()" into the "def loops(self):" function
-core/commands.py | Add "import unreal"
-core/config.py   | Add "unreal_dir = '~/CHANGEME'" under the "admin_host" line. (Replace CHANGEME with the directory location.)
-
-To disable this module, undo the changes above.
-'''
-
 import threading
 import time
 
@@ -27,7 +15,7 @@ import irc
 def check_update():
     source = httplib.get_source('https://www.unrealircd.org/docs/FAQ')
     latest = functions.between(source, 'The latest <b>Stable</b> version is <b>', '</b> which was released on ')
-    if current_version != latest:
+    if current_version() != latest:
         return latest
     else:
         return False
@@ -44,5 +32,5 @@ class loop(threading.Thread):
             if time.strftime('%I') == 12:
                 update = check_update()
                 if update:
-                    irc.DickServ.sendmsg(config.channel, 'UnrealIRCd version %s has been released.' % update)
+                    irc.DickServ.sendmsg(config.channel, 'UnrealIRCd version {0} has been released.'.format(update))
             time.sleep(45*60)
