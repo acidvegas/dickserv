93f653f68fc26cb6c9e33a6dae6f6e225422e1c3
Dionysus <acidvegas@users.noreply.github.com>
2016-10-10 22:02:43 -0400

    Update isup.py

--- dickserv/modules/isup.py
+++ dickserv/modules/isup.py
@@ -8,7 +8,11 @@ import httplib
 
 def check(url):
     source = httplib.get_source('http://isup.me/' + url)
-    if   source.find('It\'s just you.') != -1     : return 'UP'
-    elif source.find('It\'s not just you!') != -1 : return 'DOWN'
-    elif source.find('Huh?') != -1                : return 'INVALID'
-    else                                          : return 'UNKNOWN'
+    if source.find('It\'s just you.') != -1:
+        return 'UP'
+    elif source.find('It\'s not just you!') != -1:
+        return 'DOWN'
+    elif source.find('Huh?') != -1:
+        return 'INVALID'
+    else:
+        return 'UNKNOWN'
