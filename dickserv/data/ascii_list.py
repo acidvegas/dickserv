575507d27721b21e3044ff65694b4c63ace3fe03
Dionysus <acidvegas@users.noreply.github.com>
2016-10-15 06:23:16 -0400

    Delete ascii_list.py

--- dickserv/data/ascii_list.py
+++
@@ -1,6 +0,0 @@
-import glob, os
-with open('list.txt', 'w') as ascii_file:
-    count = 1
-    for i in glob.glob('ascii/*.txt'):
-        ascii_file.write('%d. %s\n' % (count, os.path.basename(i).replace('.txt', '', 1)))
-        count += 1
